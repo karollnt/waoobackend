8ec64dd397f63f74fc30ef266f148e2fc86734cf
rhonaldomaster <rhonaldomaster@gmail.com>
2016-01-16 19:18:21 -0500

    Actualizaciones varias solicitudes
    Nuevas funcionalidades para la parte de solicitudes, registro,envio de
    oferta, aceptacion de oferta, notificacion, log de ofertas

--- application/controllers/solicitudes.php
+++ application/controllers/solicitudes.php
@@ -4,10 +4,11 @@
 		public function __construct(){
 			parent::__construct();
 			$this->load->model('SolicitudesModel');
+			$this->load->model('UsuariosModel');
+			$this->load->model('MateriasModel');
 		}
 		
 		public function crearSolicitud(){
-			$this->load->model('UsuariosModel');
 			$nickname = $this->input->post('nickname');
 			$titulo = $this->input->post('titulo');
 			$descripcion = $this->input->post('descripcion');
@@ -42,6 +43,106 @@
 			$resp = array("msg"=>html_entity_decode($mensaje));
 			//echo $_GET['callback'].'('.json_encode($resp).')';
 			echo json_encode($resp);
+		}
+		
+		function solicitudesPorMateriaAsistente(){
+			$mensaje = '';
+			$nickname = $this->input->post('nickname');
+			$lm = $this->MateriasModel->listarMateriasAsesor($nickname);
+			$lm = '['.$lm.']';
+			$lista = json_decode($lm);
+			$cont1 = 0;
+			foreach($lista as $obj){
+				if($cont1==0) $cont1 = 1;
+				else $mensaje .= '|';
+				$mensaje .= $obj->nombre.';'.($this->SolicitudesModel->solicitudesAsistenteMateria($nickname,$obj->id));
+			}
+			$resp = array("msg"=>html_entity_decode($mensaje));
+			//echo $_GET['callback'].'('.json_encode($resp).')';
+			echo json_encode($resp);
+		}
+		
+		function solicitudesSinAsignar(){
+			$mensaje = '';
+			$mensaje = $this->SolicitudesModel->solicitudesSinAsignar();
+			$resp = array("msg"=>html_entity_decode($mensaje));
+			//echo $_GET['callback'].'('.json_encode($resp).')';
+			echo json_encode($resp);
+		}
+		
+		function solicitudesSinAsignarPorMateria(){
+			$mensaje = '';
+			$idmateria = $this->input->post('idmateria');
+			$mensaje = $this->SolicitudesModel->solicitudesSinAsignarPorMateria($idmateria);
+			$resp = array("msg"=>html_entity_decode($mensaje));
+			//echo $_GET['callback'].'('.json_encode($resp).')';
+			echo json_encode($resp);
+		}
+		
+		public function enviarPrecioTrabajo(){
+			$mensaje = '';
+			$nickname = $this->input->post('nickname');
+			$idtrabajo = $this->input->post('idtrabajo');
+			$valor = $this->input->post('valor');
+			$usuario = $this->UsuariosModel->usuarioObj($nickname);
+			$mensaje = $this->SolicitudesModel->enviarPrecioTrabajo($idtrabajo,$usuario->id,$valor);
+			$resp = array("msg"=>html_entity_decode($mensaje));
+			//echo $_GET['callback'].'('.json_encode($resp).')';
+			echo json_encode($resp);
+		}
+		
+		public function aceptarPrecio(){
+			$mensaje = '';
+			$idpreciotrabajo = $this->input->post("idpreciotrabajo");
+			$numcomprobante = $this->input->post("numcomprobante");
+			$mensaje = $this->SolicitudesModel->aceptarPrecio($idpreciotrabajo,$numcomprobante);
+			$resp = array("msg"=>html_entity_decode($mensaje));
+			//echo $_GET['callback'].'('.json_encode($resp).')';
+			echo json_encode($resp);
+		}
+		
+		public function asignarAsistenteTrabajo(){
+			$mensaje = '';
+			$nickname = $this->input->post('nickname');
+			$idtrabajo = $this->input->post('idtrabajo');
+			$numcomprobante = $this->input->post('numcomprobante');
+			$usuario = $this->UsuariosModel->usuarioObj($nickname);
+			$mensaje = $this->SolicitudesModel->asignarAsistenteTrabajo($usuario->id,$idtrabajo,$numcomprobante);
+			$resp = array("msg"=>html_entity_decode($mensaje));
+			//echo $_GET['callback'].'('.json_encode($resp).')';
+			echo json_encode($resp);
+		}
+		
+		private function configuracionPayU($usuario,$idtrabajo,$valor){
+			$order = array();
+			$order['notifyUrl'] = 'http://localhost'.dirname($_SERVER['REQUEST_URI']).'/OrderNotify.php';
+			$order['continueUrl'] = 'http://localhost'.dirname($_SERVER['REQUEST_URI']).'/../../layout/success.php';
+			$order['customerIp'] = '127.0.0.1';
+			$order['merchantPosId'] = OpenPayU_Configuration::getMerchantPosId();
+			$order['description'] = 'Pago de trabajo '.$idtrabajo;
+			$order['currencyCode'] = 'COP';
+			$order['totalAmount'] = $valor;
+			$order['extOrderId'] = uniqid('', true);
+			$order['products'][0]['name'] = 'Product1';
+			$order['products'][0]['unitPrice'] = $valor;
+			$order['products'][0]['quantity'] = 1;
+			$order['buyer']['email'] = $usuario->email;
+			$order['buyer']['phone'] = $usuario->celular;
+			$order['buyer']['firstName'] = $usuario->nombres;
+			$order['buyer']['lastName'] = $usuario->apellidos;
+			return $order;
+		}
+		
+		//Para payu: https://github.com/PayU/openpayu_php
+		public function pagarConPayU(){
+			$idpreciotrabajo = $this->input->post("idpreciotrabajo");
+			$valor = $this->SolicitudesModel->valorOfertaTrabajo($idpreciotrabajo);
+			$idtrabajo = $this->SolicitudesModel->idTrabajoDesdeOfertaTrabajo($idpreciotrabajo);
+			$nickname = "(SELECT nickname FROM trabajo WHERE id={$idtrabajo})";
+			$usuario = $this->UsuariosModel->usuarioObj($nickname);
+			$orden = $this->configuracionPayU($usuario,$idtrabajo,$valor);
+			$orderFormData = OpenPayU_Order::hostedOrderForm($orden);
+			echo $orderFormData;
 		}
 		
 	}
--- application/models/solicitudesModel.php
+++ application/models/solicitudesModel.php
@@ -12,4 +12,222 @@
 			return $mensaje;
 		}
 		
+		public function solicitudesUsuario($nickname){
+			$mensaje = '';
+			$this->db
+			->select("t.id,t.titulo,t.descripcion,t.fecharegistro,t.fecharesuelto,e.nombre AS nomestado,COALESCE(u2.nickname,'Sin asignar') AS asistente,m.nombre AS materia",false)
+			->from("trabajo t")
+			->join("usuarios u","u.id=t.idusuario","inner")
+			->join("estados e","e.id=t.estado","inner")
+			->join("materias m","m.id=t.idmateria","inner")
+			->join("usuarios u2","u2.id=t.idasistente","left")
+			->where("u.nickname",$nickname)
+			->order_by("t.estado","desc")
+			->order_by("t.fecharegistro","desc");
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				$cont1 = 0;
+				foreach($res->result() as $row){
+					if($cont1==0) $cont1 = 1;
+					else $mensaje .= ',';
+					$mensaje .= '{"id":"'.($row->id).'","titulo":"'.($row->titulo).'",'
+					.'"descripcion":"'.($row->descripcion).'","fecharegistro":"'.($row->fecharegistro).'",'
+					.'"fecharesuelto":"'.($row->fecharesuelto).'","asistente":"'.($row->asistente).'",'
+					.'"estado":"'.($row->nomestado).'"}';
+				}
+			}
+			return $mensaje;
+		}
+		
+		public function solicitudesAsistente($nickname){
+			$mensaje = '';
+			$this->db
+			->select("t.id,t.titulo,t.descripcion,t.fecharegistro,t.fecharesuelto,e.nombre AS nomestado,COALESCE(u2.nickname,'Sin asignar') AS asistente,m.nombre AS materia",false)
+			->from("trabajo t")
+			->join("usuarios u","u.id=t.idasistente","inner")
+			->join("estados e","e.id=t.estado","inner")
+			->join("materias m","m.id=t.idmateria","inner")
+			->join("usuarios u2","u2.id=t.idusuario","inner")
+			->where("u.nickname",$nickname)
+			->order_by("t.estado","desc")
+			->order_by("t.fecharegistro","desc");
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				$cont1 = 0;
+				foreach($res->result() as $row){
+					if($cont1==0) $cont1 = 1;
+					else $mensaje .= ',';
+					$mensaje .= '{"id":"'.($row->id).'","titulo":"'.($row->titulo).'",'
+					.'"descripcion":"'.($row->descripcion).'","fecharegistro":"'.($row->fecharegistro).'",'
+					.'"fecharesuelto":"'.($row->fecharesuelto).'","solicitante":"'.($row->solicitante).'",'
+					.'"estado":"'.($row->nomestado).'"}';
+				}
+			}
+			return $mensaje;
+		}
+		
+		public function solicitudesAsistenteMateria($nickname,$idmateria){
+			$mensaje = '';
+			$this->db->select("t.id,t.titulo,t.descripcion,t.fecharegistro,t.fecharesuelto,e.nombre AS nomestado,u.nickname AS solicitante",false)
+			->from("trabajo t")
+			->join("usuarios u","u.id=t.idasistente","inner")
+			->join("estados e","e.id=t.estado","inner")
+			->join("usuarios u2","u2.id=t.idusuario","inner")
+			->where(array("u.nickname"=>$nickname,"t.idmateria"=>$idmateria))
+			->order_by("t.estado","desc")
+			->order_by("t.fecharegistro","desc");
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				$cont1 = 0;
+				foreach($res->result() as $row){
+					if($cont1==0) $cont1 = 1;
+					else $mensaje .= ',';
+					$mensaje .= '{"id":"'.($row->id).'","titulo":"'.($row->titulo).'",'
+					.'"descripcion":"'.($row->descripcion).'","fecharegistro":"'.($row->fecharegistro).'",'
+					.'"fecharesuelto":"'.($row->fecharesuelto).'","solicitante":"'.($row->solicitante).'",'
+					.'"estado":"'.($row->nomestado).'"}';
+				}
+			}
+			else{
+				$mensaje = 'No hay solicitudes';
+			}
+			return $mensaje;
+		}
+		
+		public function solicitudesSinAsignar(){
+			$mensaje = '';
+			$this->db
+			->select("t.id,t.titulo,t.descripcion,t.fecharegistro,m.nombre AS materia,u.nickname",false)
+			->from("trabajo t")
+			->join("materias m","m.id=t.idmateria","inner")
+			->join("usuarios u","u.id=t.idusuario","inner")
+			->where("t.idasistente",0);
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				$cont1 = 0;
+				$mensaje = '[';
+				foreach($res->result() as $row){
+					if($cont1==0) $cont1 = 1;
+					else $mensaje .= ',';
+					$mensaje .= '{"id":"'.($row->id).'","titulo":"'.($row->titulo).'","materia":"'.($row->materia).'"'
+					.'"descripcion":"'.($row->descripcion).'","fecharegistro":"'.($row->fecharegistro).'","solicitante":"'.($row->nickname).'"}';
+					$mensaje .= ']';
+				}
+			}
+			else{
+				$mensaje = 'No hay solicitudes';
+			}
+			return $mensaje;
+		}
+		
+		public function solicitudesSinAsignarPorMateria($idmateria){
+			$mensaje = '';
+			$this->db
+			->select("t.id,t.titulo,t.descripcion,t.fecharegistro,m.nombre AS materia,u.nickname",false)
+			->from("trabajo t")
+			->join("materias m","m.id=t.idmateria","inner")
+			->join("usuarios u","u.id=t.idusuario","inner")
+			->where(array("t.idasistente"=>0,"t.idmateria"=>$idmateria));
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				$cont1 = 0;
+				$mensaje = '[';
+				foreach($res->result() as $row){
+					if($cont1==0) $cont1 = 1;
+					else $mensaje .= ',';
+					$mensaje .= '{"id":"'.($row->id).'","titulo":"'.($row->titulo).'","materia":"'.($row->materia).'"'
+					.'"descripcion":"'.($row->descripcion).'","fecharegistro":"'.($row->fecharegistro).'","solicitante":"'.($row->nickname).'"}';
+				}
+				$mensaje .= ']';
+			}
+			else{
+				$mensaje = 'No hay solicitudes';
+			}
+			return $mensaje;
+		}
+		
+		public function enviarPrecioTrabajo($idtrabajo,$idasistente,$valor){
+			$mensaje = '';
+			$this->db->insert('ofertatrabajo',array("idtrabajo"=>$idtrabajo,"idasistente"=>$idasistente));
+			if($this->db->affected_rows()>0) $mensaje = "Informaci&oacute;n ingresada";
+			else $mensaje = "No se pudo ingresar la informaci&oacute;n";
+			$msg = "Ha recibido una oferta para realizar su trabajo {$idtrabajo} por $".number_format($valor,0,".",",");
+			$this->notificarUsuario($msg,"(SELECT idusuario FROM trabajo WHERE id={$idtrabajo})");
+			return $mensaje;
+		}
+		
+		public function aceptarPrecio($idpreciotrabajo,$numcomprobante){
+			$mensaje = "";
+			$idtrabajo = "(SELECT idtrabajo FROM ofertatrabajo WHERE id={$idpreciotrabajo})";
+			$idasistente = "(SELECT idasistente FROM ofertatrabajo WHERE id={$idpreciotrabajo})";
+			$idusuario = "(SELECT idusuario FROM trabajo WHERE id={$idtrabajo})";
+			$this->db->where('id',$idpreciotrabajo);
+			$this->db->update('ofertatrabajo',array("estado"=>1));
+			if($this->db->affected_rows()>0){
+				$mensaje = $this->logTrabajo($idtrabajo,$idusuario,2,"Usuario escoge asistente para hacer el trabajo");
+				if(strcasecmp($mensaje,"Informaci&oacute;n actualizada")==0){
+					$mensaje = $this->asignarAsistenteTrabajo($idtrabajo,$idasistente,$numcomprobante);
+					$this->notificarUsuario("Su oferta para el trabajo {$idtrabajo} ha sido aceptada",$idasistente);
+				}
+				else $mensaje = "No se pudo actualizar la informaci&oacute;n";
+			}
+			else $mensaje = "No se pudo actualizar la informaci&oacute;n";
+			return $mensaje;
+		}
+		
+		public function asignarAsistenteTrabajo($idasistente,$idtrabajo,$numcomprobante){
+			$mensaje = '';
+			$this->db->where('id',$idtrabajo);
+			$this->db->update('trabajo',array("idasistente"=>$idasistente,"estado"=>2,"numcomprobante"=>$numcomprobante));
+			if($this->db->affected_rows()>0) $mensaje = "Informaci&oacute;n actualizada";
+			else $mensaje = "No se pudo actualizar la informaci&oacute;n";
+			return $mensaje;
+		}
+		
+		public function logTrabajo($idtrabajo,$idusuario,$tipo,$desc){
+			$mensaje = '';
+			$this->db->insert('trabajolog',array("idtrabajo"=>$idtrabajo,"idusuario"=>$idusuario,"tipolog"=>$tipo,"descripcion"=>$desc));
+			if($this->db->affected_rows()>0) $mensaje = "Informaci&oacute;n ingresada";
+			else $mensaje = "No se pudo ingresar la informaci&oacute;n";
+			return $mensaje;
+		}
+		
+		public function notificarUsuario($mensaje,$idusuario){
+			$mensaje = '';
+			$this->db->insert('trabajolog',array("mensaje"=>$mensaje,"idusuario"=>$idusuario));
+			if($this->db->affected_rows()>0) $mensaje = "Informaci&oacute;n ingresada";
+			else $mensaje = "No se pudo ingresar la informaci&oacute;n";
+			return $mensaje;
+		}
+		
+		public function valorOfertaTrabajo($idoferta){
+			$valor = 0;
+			$this->db
+			->select("valor")
+			->from("ofertatrabajo")
+			->where("id",$idoferta);
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				foreach($res->result() as $row){
+					$valor = $row->valor;
+				}
+			}
+			return $valor;
+		}
+		
+		public function idTrabajoDesdeOfertaTrabajo($idoferta){
+			$id = 0;
+			$this->db
+			->select("idtrabajo")
+			->from("ofertatrabajo")
+			->where("id",$idoferta);
+			$res = $this->db->get();
+			if($res->num_rows()>0){
+				foreach($res->result() as $row){
+					$id = $row->idtrabajo;
+				}
+			}
+			return $id;
+		}
+		
 	}
