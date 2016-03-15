326358e7f51dcd890353b5391b2f7a69f3840245
rhonaldomaster <rhonaldomaster@gmail.com>
2016-03-15 12:18:53 -0500

    Cambio nombre test 2

---
+++ application/controllers/Bancos.php
@@ -0,0 +1,16 @@
+<?php
+	class Bancos extends CI_Controller{
+		public function __construct(){
+			parent::__construct();
+			$this->load->model('BancosModel');
+		}
+
+		public function listaBancos(){
+			$mensaje = "";
+			$msg = $this->BancosModel->listaBancos();
+			if(strcasecmp($msg,"")==0) $mensaje = '{"error":"No se encontraron resultados"}';
+			else $mensaje = '{"bancos":['.$msg.']}';
+			echo $mensaje;
+		}
+
+	}
