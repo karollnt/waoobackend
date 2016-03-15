d1d5aeb09fa4bbd47f7b3b92aff6e4b7d13c7adc
Rhonalf <rhonaldomaster@gmail.com>
2016-03-15 14:24:21 -0500

    Update Usuarios.php

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -2,7 +2,7 @@
 	class Usuarios extends CI_Controller{
 
 		private $errores = array(
-			'usuex'=>"E01001: Nombre de usuario ya registrado",
+			'usuex'=>"E01001: Nombre de usuario ya registrados",
 			'usuv'=>"Debe escribir un nombre de usuario",
 			'clavev'=>"E01003: Debe escribir una clave",
 			'clavec'=>"E01004: Clave demasiado corta",
