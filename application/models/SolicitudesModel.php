8479e03753f65b154f70e035be0d5544e6a50e20
rhonaldomaster <rhonaldomaster@gmail.com>
2016-03-22 17:15:33 -0500

    test

--- application/models/SolicitudesModel.php
+++ application/models/SolicitudesModel.php
@@ -24,22 +24,7 @@
 		public function ingresarArchivos($idtrabajo,$idusuario,$datos){
 			$mensaje = '';
 			$buckName = 'waoofiles';
-			$bucket = $this->s3->getBucket($buckName);
-			if($bucket !==false) ;
-			else{
-				$this->s3->putBucket($buckName,$this->S3->ACL_PUBLIC_READ);
-				$bucket = $this->s3->getBucket($buckName);
-			}
-			foreach($datos as $i=>$v){
-				/*$dats = array('idtrabajo'=>$idtrabajo,'idusuario'=>$idusuario,
-				'archivo'=>$v['archivo'],'tipoarchivo'=>$v['tipoarchivo'],'extension'=>$v['extension']);*/
-				//$this->db->insert('trabajoarchivos',$dats);
-				$putf = $this->S3->putObject($v['archivo'],$bucket,"upl-{$idusuario}-{$i}.".$v['extension'],$this->S3->ACL_PUBLIC_READ);
-				if($putf) $mensaje = "Se ha guardado el archivo";
-				else $mensaje = "No se pudo ingresar la informaci&oacute;n";
-				/*if($this->db->affected_rows()>0) $mensaje = "Se ha guardado el archivo";
-				else $mensaje = "No se pudo ingresar la informaci&oacute;n";*/
-			}
+			echo $this->S3->ACL_PUBLIC_READ;
 			return $mensaje;
 		}
 
