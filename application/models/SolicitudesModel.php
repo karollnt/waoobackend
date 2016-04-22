4a51571a4fef87626b8d12951e90bf5ec090c842
rhonaldomaster <rhonaldomaster@gmail.com>
2016-04-22 16:14:52 -0500

    revirtiendo

--- application/models/SolicitudesModel.php
+++ application/models/SolicitudesModel.php
@@ -236,13 +236,12 @@
 			if($verif) $aupd["valor"] = 0;
 			$this->db->where('id',$idpreciotrabajo);
 			$this->db->update('ofertatrabajo',$aupd);
-			if(!$verif){
-				$this->mp->sandbox_mode(true);
-				$payment = $this->mp->post("/v1/payments", $datosmp);
-				var_dump($payment);
-			}
 			if($this->db->affected_rows()>0){
-
+				if(!$verif){
+					$this->mp->sandbox_mode(true);
+					$payment = $this->mp->post("/v1/payments", $datosmp);
+					var_dump($payment);
+				}
 				$mensaje = $this->logTrabajo($idtrabajo,$idusuario,2,"Usuario escoge asistente para hacer el trabajo");
 				if(strcasecmp($mensaje,"Informaci&oacute;n actualizada")==0){
 					$mensaje = $this->asignarAsistenteTrabajo($idtrabajo,$idasistente,$numcomprobante);
