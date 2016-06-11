d981a8f381f12c36461e1c1f10264f9ed5fda6e7
rhonaldomaster <rhonaldomaster@gmail.com>
2016-06-11 17:56:35 -0500

    bug fix

--- application/controllers/Solicitudes.php
+++ application/controllers/Solicitudes.php
@@ -233,6 +233,7 @@
 					}
 				}
 			}
+			$usuario = $this->UsuariosModel->usuarioObj($nickname);
 			$datos = array("idusuario"=>($usuario->id),"idtrabajo"=>$idtrabajo);
 			if($datos2!=null) $this->SolicitudesModel->ingresarArchivos($idtrabajo,$datos['idusuario'],$datos2);
 			$mensaje = $this->SolicitudesModel->enviarSolucion($datos);
