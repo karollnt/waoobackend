275e8329fa853dead01d6d0b15cd0d33b96b68b9
Karoll Navas <futlaos@gmail.com>
2017-09-25 10:11:16 -0500

    Update SolicitudesModel.php

--- application/models/SolicitudesModel.php
+++ application/models/SolicitudesModel.php
@@ -420,9 +420,10 @@
       $mensaje = '';
       $this->load->model('UsuariosModel');
       $this->db
-      ->select("otr.id,otr.valor,u.nickname,otr.idasistente,u.nombres",false)
+      ->select("otr.id,otr.valor,u.nickname,otr.idasistente,du.descripcion",false)
       ->from("ofertatrabajo otr")
       ->join("usuarios u","u.id=otr.idasistente","inner")
+      ->join("datos_usuario du","du.id_usuario=otr.idasistente","inner")
       ->where("idtrabajo",$idtrabajo);
       $res = $this->db->get();
       if($res->num_rows()>0){
@@ -432,7 +433,7 @@
           $verif = $this->verificarPrimerTrabajo($row->idasistente);
           if($cont1==0) $cont1 = 1;
           else $mensaje .= ',';
-          $mensaje .= '{"id":"'.($row->id).'","valor":"'.($verif==true?0:$row->valor).'","asistente":"'.($row->nickname).'","calificacion":"'.($calif).'"}';
+          $mensaje .= '{"id":"'.($row->id).'","valor":"'.($verif==true?0:$row->valor).'","asistente":"'.($row->nickname).'","calificacion":"'.($calif).'","descripcion":"'.($row->descripcion).'"}';
         }
       }
       return $mensaje;
