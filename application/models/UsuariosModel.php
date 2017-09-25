52a51e4de6853cc275059f52741b52fb79315e94
Karoll Navas <futlaos@gmail.com>
2017-09-25 11:48:08 -0500

    Update UsuariosModel.php

--- application/models/UsuariosModel.php
+++ application/models/UsuariosModel.php
@@ -604,11 +604,8 @@
     }
 // Nueva funcion que trae comentarios por id
      public function MostrarComentarios($id){
-            $this->db->select('*');
-            $this->db->from("raiting");
-            $this->db->where('idasistente', $id);
-            $result = $this->db->get();
-	    return $result->result_array();
+           
+	    return "hola";
 	     
   }	
 
