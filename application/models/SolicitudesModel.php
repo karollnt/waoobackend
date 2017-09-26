3605d6bf87cdedb6f602af0c027bfd7b894c0b47
Karoll Navas <futlaos@gmail.com>
2017-09-26 09:31:27 -0500

    Update SolicitudesModel.php

--- application/models/SolicitudesModel.php
+++ application/models/SolicitudesModel.php
@@ -432,7 +432,7 @@
         foreach($res->result() as $row){
           $calif = $this->UsuariosModel->calificacionAsesor($row->nickname);
           $verif = $this->verificarPrimerTrabajo($row->idasistente);
-          $comen = array("mal profesor", "buen trabajo", "saque 5")
+          $comen = array("mal profesor", "buen trabajo", "saque 5");
           if($cont1==0) $cont1 = 1;
           else $mensaje .= ',';
          $mensaje .= '{"id":"'.($row->id).'","valor":"'.($verif==true?0:$row->valor).'","asistente":"'.($row->nickname).'","calificacion":"'.($calif).'","nombre":"'.($row->nombres)." ".($row->apellidos).'","descripcion":"'.($row->descripcion).'","institucion":"'.($row->institucionedu).'","nivel":"'.($row->nivel_edu).'","comentarios":"'.($comen).'"}';
