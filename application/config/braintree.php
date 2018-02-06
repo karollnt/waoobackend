23d52728aa33b1fd0ac573e508dcdc20133a5dce
Karoll Navas <futlaos@gmail.com>
2018-02-05 19:48:03 -0500

    cambio a waootechnology
    esperando q sirva

--- application/config/braintree.php
+++ application/config/braintree.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['braintree_merchant_id'] = '9qycxmb5sfthw6kw';
-$config['braintree_public_key']  = 'mfmkj2n4bpyqx498';
-$config['braintree_private_key'] = 'e3fe22809ce8f9a2b43331d014286777';
+$config['braintree_merchant_id'] = '7zxcdsmmwszxck5g';
+$config['braintree_public_key']  = 'tf53x3gqd28x7rwd';
+$config['braintree_private_key'] = '38e5e8f5f566d373b07f62f189f7bf3f';
 $config['braintree_environment'] = 'sandbox'; //production or sandbox
